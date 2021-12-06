eb468c9d494d01f798225d38b8931653dcc85d69
Alan Wills <alan@treespleasegames.com>
2021-12-06 20:25:28 +0000

    Adding location services package

---
+++ LocationServices/Scripts/Debug/LocationServicesDebugMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Celeste.LocationServices
+{
+    public class LocationServicesDebugMenu : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
