98ff84abbaec8c958c719165288e5ca94400023b
Alan Wills <alawills@googlemail.com>
2022-11-07 16:33:22 +0000

    Slight tweak to audio clip event raiser

--- Events/Scripts/Audio/AudioClipEventRaiser.cs
+++ Events/Scripts/Audio/AudioClipEventRaiser.cs
@@ -5,5 +5,15 @@ namespace Celeste.Events
     [AddComponentMenu("Celeste/Events/Audio/Audio Clip Event Raiser")]
     public class AudioClipEventRaiser : ParameterisedEventRaiser<AudioClip, AudioClipEvent>
     {
+        #region Properties and Fields
+
+        [SerializeField] private AudioClip presetAudioClip;
+
+        #endregion
+
+        public void RaiseWithPreset()
+        {
+            Raise(presetAudioClip);
+        }
     }
 }
