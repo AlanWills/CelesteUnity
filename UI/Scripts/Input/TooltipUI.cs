8c81495b55f51300aae066c7792dc1d80df54912
Alan Wills <alawills@googlemail.com>
2022-12-10 11:59:22 +0000

    Cleaned up commented out code

--- UI/Scripts/Input/TooltipUI.cs
+++ UI/Scripts/Input/TooltipUI.cs
@@ -9,7 +9,6 @@ namespace Celeste.UI.Input
     {
         #region Properties and Fields
 
-        [SerializeField] private Canvas tooltipCanvas;
         [SerializeField] private Camera worldSpaceCamera;
         [SerializeField] private RectTransform tooltipRoot;
         [SerializeField] private TextMeshProUGUI tooltipText;
@@ -20,17 +19,7 @@ namespace Celeste.UI.Input
 
         public void ShowTooltip(TooltipArgs tooltipArgs)
         {
-            //Vector2 pos = tooltipArgs.position;  // get the game object position
-            //Vector2 viewportPoint = worldSpaceCamera.WorldToViewportPoint(pos);  //convert game object position to VievportPoint
-
-            //// set MIN and MAX Anchor values(positions) to the same position (ViewportPoint)
-            //tooltipRoot.anchorMin = viewportPoint;
-            //tooltipRoot.anchorMax = viewportPoint;
-
             tooltipText.text = tooltipArgs.text;
-            //tooltipRoot.position = tooltipArgs.position;
-            //Vector3 position = worldSpaceCamera.WorldToScreenPoint(new Vector3(tooltipArgs.position.x, tooltipArgs.position.y, -worldSpaceCamera.transform.position.z));
-            //tooltipRoot.transform.position = tooltipCanvas.GetComponent<RectTransform>().InverseTransformVector(tooltipArgs.position);
             tooltipRoot.transform.position = tooltipArgs.isWorldSpace ? 
                 worldSpaceCamera.WorldToScreenPoint(new Vector3(tooltipArgs.position.x, tooltipArgs.position.y, -worldSpaceCamera.transform.position.z)) : tooltipArgs.position;
             tooltipRoot.gameObject.SetActive(true);
