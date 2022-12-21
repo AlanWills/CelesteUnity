a9b0b784bca08966bd2607e561bf844976fb836f
Alan Wills <alawills@googlemail.com>
2022-12-21 12:35:32 +0000

    Added rect gizmo script to help arrangement of game objects

---
+++ Tools/Scripts/Gizmos/RectGizmo.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Celeste.Tools
+{
+    [AddComponentMenu("Celeste/Tools/Gizmos/Rect Gizmo")]
+    public class RectGizmo : MonoBehaviour
+    {
+        [SerializeField] private Vector2 size;
+
+        private void OnDrawGizmos()
+        {
+            Vector3 position = transform.position;
+            Vector3 topRight = position + new Vector3(size.x * 0.5f, size.y * 0.5f, 0);
+            Vector3 bottomRight = position + new Vector3(size.x * 0.5f, -size.y * 0.5f, 0);
+            Vector3 bottomLeft = position + new Vector3(-size.x * 0.5f, -size.y * 0.5f, 0);
+            Vector3 topLeft = position + new Vector3(-size.x * 0.5f, size.y * 0.5f, 0);
+
+            Color originalGizmoColour = Gizmos.color;
+            Gizmos.color = Color.yellow;
+
+            Gizmos.DrawLine(topRight, bottomRight);
+            Gizmos.DrawLine(bottomRight, bottomLeft);
+            Gizmos.DrawLine(bottomLeft, topLeft);
+            Gizmos.DrawLine(topLeft, topRight);
+
+            Gizmos.color = originalGizmoColour;
+        }
+    }
+}
