3862b2be5f9476e3c7cbbe67caed187192cadf0c
Alan Wills <alawills@googlemail.com>
2023-01-29 10:44:50 +0000

    Added guaranteed events, now working on rolling them out

---
+++ Events/Scripts/Abstract/GuaranteedEvent.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Celeste.Events
+{
+    [Serializable]
+    public class GuaranteedEvent : IEvent
+    {
+        #region Properties and Fields
+
+        private Event ObjectEvent
+        {
+            get
+            {
+                if (objectEvent == null)
+                {
+                    objectEvent = ScriptableObject.CreateInstance<Event>();
+                }
+
+                return objectEvent;
+            }
+        }
+
+        [SerializeField] private Event objectEvent;
+
+        #endregion
+
+        public ICallbackHandle AddListener(IEventListener listener)
+        {
+            return ObjectEvent.AddListener(listener);
+        }
+
+        public ICallbackHandle AddListener(UnityAction unityAction)
+        {
+            return ObjectEvent.AddListener(unityAction);
+        }
+
+        public void RemoveListener(UnityAction callback)
+        {
+            ObjectEvent.RemoveListener(callback);
+        }
+
+        public void RemoveListener(IEventListener listener)
+        {
+            ObjectEvent.RemoveListener(listener);
+        }
+
+        public void RemoveListener(ICallbackHandle callbackHandle)
+        {
+            ObjectEvent.RemoveListener(callbackHandle);
+        }
+
+        public void Invoke()
+        {
+            ObjectEvent.Invoke();
+        }
+
+        public void InvokeSilently()
+        {
+            ObjectEvent.InvokeSilently();
+        }
+    }
+}
---
+++ Events/Scripts/Abstract/GuaranteedParameterisedEvent.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Celeste.Events
+{
+    [Serializable]
+    public class GuaranteedParameterisedEvent<TEvent, TParam> : IEvent<TParam> where TEvent : ParameterisedEvent<TParam>
+    {
+        #region Properties and Fields
+
+        private TEvent ObjectEvent
+        {
+            get
+            {
+                if (objectEvent == null)
+                {
+                    objectEvent = ScriptableObject.CreateInstance<TEvent>();
+                }
+
+                return objectEvent;
+            }
+        }
+
+        [SerializeField] private TEvent objectEvent;
+
+        #endregion
+
+        public ICallbackHandle AddListener(IEventListener<TParam> listener)
+        {
+            return ObjectEvent.AddListener(listener);
+        }
+
+        public ICallbackHandle AddListener(UnityAction<TParam> unityAction)
+        {
+            return ObjectEvent.AddListener(unityAction);
+        }
+
+        public void RemoveListener(UnityAction<TParam> callback)
+        {
+            ObjectEvent.RemoveListener(callback);
+        }
+
+        public void RemoveListener(IEventListener<TParam> listener)
+        {
+            ObjectEvent.RemoveListener(listener);
+        }
+
+        public void RemoveListener(ICallbackHandle callbackHandle)
+        {
+            ObjectEvent.RemoveListener(callbackHandle);
+        }
+
+        public void Invoke(TParam param)
+        {
+            ObjectEvent.Invoke(param);
+        }
+
+        public void InvokeSilently(TParam param)
+        {
+            ObjectEvent.InvokeSilently(param);
+        }
+    }
+}
