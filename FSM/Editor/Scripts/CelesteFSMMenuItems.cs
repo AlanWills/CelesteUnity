7aef38488ef5b8cbcfe3875e0ca76edea8a1bca3
Alan Wills <alawills@googlemail.com>
2023-04-09 21:32:55 +0100

    Working on some more options UI and experimenting with context menu items

---
+++ FSM/Editor/Scripts/CelesteFSMMenuItems.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+
+namespace CelesteEditor.FSM
+{
+    public static class CelesteFSMMenuItems
+    {
+        private const string CREATE_FSM_RUNTIME_MENU_PATH = "GameObject/Celeste/FSM/FSM Runtime";
+
+        [MenuItem(CREATE_FSM_RUNTIME_MENU_PATH, true)]
+        public static bool ValidateCreateFSMRuntimeContextMenuItem()
+        {
+            return true;
+        }
+
+        [MenuItem(CREATE_FSM_RUNTIME_MENU_PATH)]
+        public static void CreateFSMRuntimeContextMenuItem()
+        {
+            string assetGuid = Selection.assetGUIDs[0];
+            var a = AssetDatabase.LoadAssetAtPath<UnityEngine.Object>(AssetDatabase.GUIDToAssetPath(assetGuid));
+            var o = Selection.activeObject;
+            UnityEngine.Debug.Log("Here");
+        }
+    }
+}
---
+++ Options/Scripts/UI/FloatOptionUIController.cs
@@ -0,0 +1,49 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Celeste.Options.UI
+{
+    [AddComponentMenu("Celeste/Options/UI/Float Option UI Controller")]
+    public class FloatOptionUIController : MonoBehaviour
+    {
+        #region Properties and Fields
+
+        [Header("Data")]
+        [SerializeField] private FloatOption floatOption;
+
+        [Header("UI Elements")]
+        [SerializeField] private TextMeshProUGUI floatOptionLabel;
+        [SerializeField] private Slider floatOptionSlider;
+        [SerializeField] private TextMeshProUGUI floatOptionValue;
+
+        #endregion
+
+        #region Unity Methods
+
+        private void OnEnable()
+        {
+            floatOptionLabel.text = floatOption.DisplayName;
+            floatOptionSlider.value = floatOption.Value;
+            floatOptionValue.text = floatOption.Value.ToString();
+            floatOptionSlider.onValueChanged.AddListener(OnFloatValueChanged);
+        }
+
+        private void OnDisable()
+        {
+            floatOptionSlider.onValueChanged.RemoveListener(OnFloatValueChanged);
+        }
+
+        #endregion
+
+        #region Callbacks
+
+        public void OnFloatValueChanged(float newValue)
+        {
+            floatOption.Value = newValue;
+            floatOptionValue.text = floatOption.Value.ToString();
+        }
+
+        #endregion
+    }
+}
