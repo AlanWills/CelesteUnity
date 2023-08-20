2236ab57c120da5fc07a923a67210d829b8e6966
Alan Wills <alawills@googlemail.com>
2023-08-20 14:43:34 +0100

    Added new object field to wrap unity object field API

---
+++ Tools/Editor/Scripts/GUILayout/ObjectField.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace CelesteEditor
+{
+    public partial class CelesteEditorGUILayout
+    {
+        public static T ObjectField<T>(T obj) where T : Object
+        {
+            return EditorGUILayout.ObjectField(obj, typeof(T), false) as T;
+        }
+    }
+}
