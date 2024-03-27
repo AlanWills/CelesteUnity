2009d95631aa6eb247a7b17b899fe8acb53ca553
Alan Wills <alawills@googlemail.com>
2024-03-27 14:11:14 +0000

    Adding a bugfix to use the runtime platform to find the correct subfolder in the addressables runtime path for the cached asset bundles

--- Loading/Scripts/Jobs/EnableBundledAddressablesLoadJob.cs
+++ Loading/Scripts/Jobs/EnableBundledAddressablesLoadJob.cs
@@ -17,13 +17,31 @@ namespace Celeste.Loading
     {
         #region Properties and Fields
 
+        [SerializeField] private List<ValueTuple<RuntimePlatform, string>> runtimePlatformDirectoryLookup = new List<ValueTuple<RuntimePlatform, string>>()
+        {
+            new ValueTuple<RuntimePlatform, string>(RuntimePlatform.Android, "Android"),
+            new ValueTuple<RuntimePlatform, string>(RuntimePlatform.IPhonePlayer, "iOS"),
+            new ValueTuple<RuntimePlatform, string>(RuntimePlatform.WebGLPlayer, "WebGL"),
+            new ValueTuple<RuntimePlatform, string>(RuntimePlatform.WindowsPlayer, "StandaloneWindows64"),
+        };
+
         private CachedAssetBundles cachedAssetBundles = new CachedAssetBundles();
 
         #endregion
 
         public override IEnumerator Execute(Action<float> setProgress, Action<string> setOutput)
         {
-            var bundleCacheFileURL = GetCachedAssetBundlesPath();
+            RuntimePlatform platform = Application.platform;
+            Debug.Log($"Attempting to load cached asset bundle info for platform '{platform}'.");
+
+            var entry = runtimePlatformDirectoryLookup.Find(x => x.Item1 == platform);
+            Debug.Assert(!string.IsNullOrEmpty(entry.Item2), $"Failed to find a pre-specified addressables sub-folder for platform '{platform}'.");
+
+            string subFolder = string.IsNullOrEmpty(entry.Item2) ? platform.ToString() : entry.Item2;
+            Debug.Log($"Using addressables sub-folder '{subFolder}'.");
+
+            var bundleCacheFileURL = GetCachedAssetBundlesPath(subFolder);
+
 #if (UNITY_IOS || UNITY_ANDROID) && !UNITY_EDITOR
             var url = bundleCacheFileURL;
 #else
@@ -71,16 +89,15 @@ namespace Celeste.Loading
             return location.InternalId;
         }
 
-        private static string GetCachedAssetBundlesPath()
+        private static string GetCachedAssetBundlesPath(string addressablesRuntimePathSubFolder)
         {
 #if UNITY_EDITOR
-            // Need to do this to support using built addressables in the Unity
+            // Need to do this to support using built addressables in Unity
             // Since stuff only gets copied to StreamingAssets during buildtime (plus it would be messy for source control)
             // we have to redirect where we get the CachedAssetBundles info from
             return Path.Combine(CelesteEditor.Tools.AddressablesUtility.GetAddressablesLocalBuildPath(), "CachedAssetBundles.json");
 #else
-            return Path.Combine(Addressables.RuntimePath, "CachedAssetBundles.json");
-            //return Path.Combine(Application.streamingAssetsPath, "CachedAssetBundles.json");
+            return Path.Combine(Addressables.RuntimePath, addressablesRuntimePathSubFolder, "CachedAssetBundles.json");
 #endif
         }
 
