a280c246642e8a5f37226481e2b9244d094a369f
Alan Wills <alawills@googlemail.com>
2024-07-21 15:17:37 +0100

    Save Debug Menu

--- Persistence/Scripts/Debug/SaveDebugMenu.cs
+++ Persistence/Scripts/Debug/SaveDebugMenu.cs
@@ -1,5 +1,7 @@
 using Celeste.Debug.Menus;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using UnityEngine;
 
 namespace Celeste.Persistence
@@ -7,15 +9,26 @@ namespace Celeste.Persistence
     [CreateAssetMenu(fileName = nameof(SaveDebugMenu), menuName = CelesteMenuItemConstants.PERSISTENCE_MENU_ITEM + "Debug/Save Debug Menu", order = CelesteMenuItemConstants.PERSISTENCE_MENU_ITEM_PRIORITY)]
     public class SaveDebugMenu : DebugMenu
     {
+        [SerializeField]
+        private List<string> ignoreFiles = new List<string>()
+        {
+            "global-metadata.dat",
+            "System.Data.dll-resources.dat",
+            "mscorlib.dll-resources.dat"
+        };
+
         protected override void OnDrawMenu()
         {
-            string[] filesInPersistentData = Directory.GetFiles(Application.persistentDataPath, "*.dat", SearchOption.AllDirectories);
+            var filesInPersistentData = Directory.GetFiles(Application.persistentDataPath, "*.*", SearchOption.AllDirectories).Where(x => !ignoreFiles.Contains(Path.GetFileName(x)));
 
-            if (GUILayout.Button("Delete All"))
+            if (GUILayout.Button("Delete All '.dat' Files"))
             {
                 foreach (string file in filesInPersistentData)
                 {
-                    File.Delete(file);
+                    if (file.EndsWith(".dat", System.StringComparison.Ordinal))
+                    {
+                        File.Delete(file);
+                    }
                 }
             }
 
