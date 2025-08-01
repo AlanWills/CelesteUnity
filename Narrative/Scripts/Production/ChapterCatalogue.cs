64f6d518aad158bc2319eddd2b961495f04f653a
Alan Wills <alawills@googlemail.com>
2025-08-01 11:05:36 +0200

    Adding in automatic chapter import on the catalogue

--- Narrative/Scripts/Production/ChapterCatalogue.cs
+++ Narrative/Scripts/Production/ChapterCatalogue.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 namespace Celeste.Narrative
 {
     [CreateAssetMenu(fileName = nameof(ChapterCatalogue), menuName = CelesteMenuItemConstants.NARRATIVE_MENU_ITEM + "Production/Chapter Catalogue", order = CelesteMenuItemConstants.NARRATIVE_MENU_ITEM_PRIORITY)]
-    public class ChapterCatalogue : ListScriptableObject<Chapter>
+    public class ChapterCatalogue : ListScriptableObject<Chapter>, IAutomaticImportSettings
     {
+        public AutomaticImportBehaviour ImportBehaviour => importBehaviour;
+
+        [SerializeField] private AutomaticImportBehaviour importBehaviour = AutomaticImportBehaviour.ImportAssetsInCatalogueDirectoryAndSubDirectories;
+        
         public Chapter FindByGuid(int guid)
         {
             return FindItem(x => x.Guid == guid);
