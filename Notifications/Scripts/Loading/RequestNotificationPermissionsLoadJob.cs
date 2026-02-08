31671898dedfb718966b4f4081e7ea6bae4cb084
Alan Wills <alawills@googlemail.com>
2026-02-08 21:45:14 +0000

    Added extra log section to assert

--- Notifications/Scripts/Loading/RequestNotificationPermissionsLoadJob.cs
+++ Notifications/Scripts/Loading/RequestNotificationPermissionsLoadJob.cs
@@ -13,7 +13,7 @@ namespace Celeste.Notifications.Loading
 
         public override IEnumerator Execute(Action<float> setProgress, Action<string> setOutput)
         {
-            UnityEngine.Debug.Assert(notificationRecord.IsInitialized, $"Attempting to request notification permissions when the {nameof(NotificationRecord)} is not initialized!");
+            UnityEngine.Debug.Assert(notificationRecord.IsInitialized, $"Attempting to request notification permissions when the {nameof(NotificationRecord)} is not initialized!", CelesteLog.Notifications);
             UnityEngine.Debug.Log($"Beginning notification permissions request (requested: {notificationRecord.PermissionsRequested} & current permissions: {notificationRecord.PermissionsGranted}).", CelesteLog.Notifications);
             yield return notificationRecord.RequestPermissions();
         }
